442acfba823f37628a78c977eda62c1ec903716c
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-10-24 11:29:38 +0200

    chore: setup liquid's prettier extension

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,12 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     printWidth: 100,
+    overrides: [
+        {
+            files: ['*.liquid'],
+            options: {
+                singleQuote: false,
+            },
+        },
+    ],
 };
